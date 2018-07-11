da9c27ac2a252ddff2215784d1382bc58f9346b0
Marcin Wojtania <marcin.wojtania@gmail.com>
2018-07-11 16:05:20 +0200

    Updated HeaderSearch test after connecting component to redux

--- src/tests/components/HeaderSearch.test.js
+++ src/tests/components/HeaderSearch.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import HeaderSearch from '../../components/HeaderSearch';
+import { HeaderSearch } from '../../components/HeaderSearch';
 import { history } from '../../routers/AppRouter';
 
 test('should correctly render HeaderSearch', () => {
   const wrapper = shallow(<HeaderSearch />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should correctly set default searchText prop', () => {
+  const searchText = 'simple text';
+  const wrapper = shallow(<HeaderSearch searchText={searchText} />);
+  expect(wrapper.state('queryString')).toBe(searchText);
 });
 
 test('should set search text on input change', () => {
@@ -22,10 +28,12 @@ test('should set search text on input change', () => {
 test('should properly pushed new search text into current location', () => {
   const searchText = 'testing word';
   const searchUrl = `/szukaj/${searchText}`;
-  const wrapper = shallow(<HeaderSearch searchText={searchText}/>);
+  const setSearchFilter = jest.fn();
+  const wrapper = shallow(<HeaderSearch searchText={searchText} setSearchFilter={setSearchFilter}/>);
   wrapper.find('form').simulate('submit', {
     preventDefault: () => {}
   });
   expect(wrapper.state('queryString')).toBe('');
   expect(history.location.pathname).toBe(searchUrl);
+  expect(setSearchFilter).toHaveBeenCalledWith(searchText);
 });
