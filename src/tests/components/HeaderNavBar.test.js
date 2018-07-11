9563a02f7897c7b68c498cc812fdedfcdee9dc4e
Marcin Wojtania <marcin.wojtania@gmail.com>
2018-07-11 17:41:40 +0200

    Updated tests for both header and sub header navs

--- src/tests/components/HeaderNavBar.test.js
+++ src/tests/components/HeaderNavBar.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import HeaderNavBar from '../../components/HeaderNavBar';
+import { HeaderNavBar } from '../../components/HeaderNavBar';
 
 test('should correctly render HeaderNavBar', () => {
   const wrapper = shallow(<HeaderNavBar />);
--- src/tests/components/SubHeaderNavBar.test.js
+++ src/tests/components/SubHeaderNavBar.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import SubHeaderNavBar from '../../components/SubHeaderNavBar';
+import { SubHeaderNavBar } from '../../components/SubHeaderNavBar';
 
 test('should correctly render SubHeaderNavBar', () => {
   const wrapper = shallow(<SubHeaderNavBar />);
